6a6488ebd9c10c14aef6fd237d3d7368772c262d
General-Monish <monissam28@gmail.com>
2024-03-25 00:40:57 +0530

    working now properly than earlier

--- Coursera/Scripts/Spawner.cs
+++ Coursera/Scripts/Spawner.cs
@@ -25,6 +25,14 @@ public class Spawner : MonoBehaviour
     int maxSpawnX;
     int maxSpawnY;
 
+    // Collision free spwan support
+    const int maxSpawnTries = 20;
+    float playerColliderHalfWidth;
+    float playerColliderHalfHeight;
+    Vector2 min=new Vector2();
+    Vector2 max=new Vector2();
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,6 +46,13 @@ public class Spawner : MonoBehaviour
         spawnTimer.Duration = Random.Range(minSpawnDelay, maxSpawnDelay);
         spawnTimer.Run();
 
+        // spawn and destroy a player to cache collider values
+        GameObject tempPlayer=Instantiate<GameObject>(
+            playerPrefab,Vector3.zero,Quaternion.identity);
+        BoxCollider2D collider=tempPlayer.GetComponent<BoxCollider2D>();
+        playerColliderHalfWidth=collider.size.x/2;
+        playerColliderHalfHeight=collider.size.y/2;
+        Destroy(tempPlayer);
     }
 
     // Update is called once per frame
@@ -55,23 +70,52 @@ public class Spawner : MonoBehaviour
     {
         Vector3 location=new Vector3(Random.Range(minSpawnX,maxSpawnX),Random.Range(minSpawnY,maxSpawnY),-Camera.main.transform.position.z);
         Vector3 worldLocation=Camera.main.ScreenToWorldPoint(location);
-        GameObject player = Instantiate(playerPrefab) as GameObject;
-        player.transform.position = worldLocation;
-
-        // setting random sprite for new player
-        GameObject players;
-        int spriteNumber = Random.Range(0, 3);
-        if(spriteNumber == 0)
+       
+        SetMinAndMax(worldLocation);
+        int spawntries = 1;
+        while (Physics2D.OverlapArea(min, max) != null && spawntries <= maxSpawnTries)
         {
-            players =Instantiate<GameObject>(sprite1,worldLocation,Quaternion.identity);
-        }else if(spriteNumber == 1)
-        {
-            players = Instantiate<GameObject>(sprite2, worldLocation, Quaternion.identity);
-        }
-        else {
+            // changinglocations and pointing new rectangles
+            location.x=Random.Range(minSpawnX,maxSpawnX);
+            location.y=Random.Range(minSpawnY, maxSpawnY);
+            worldLocation = Camera.main.ScreenToWorldPoint(location);
+            SetMinAndMax (worldLocation);
 
-            players = Instantiate<GameObject>(sprite3, worldLocation, Quaternion.identity);
+            spawntries++;
         }
 
+        if(Physics2D.OverlapArea(min, max) == null)
+        {
+            GameObject player = Instantiate(playerPrefab) as GameObject;
+            player.transform.position = worldLocation;
+            // setting random sprite for new player
+            GameObject players;
+            int spriteNumber = Random.Range(0, 3);
+            if (spriteNumber == 0)
+            {
+                players = Instantiate<GameObject>(sprite1, worldLocation, Quaternion.identity);
+            }
+            else if (spriteNumber == 1)
+            {
+                players = Instantiate<GameObject>(sprite2, worldLocation, Quaternion.identity);
+            }
+            else
+            {
+
+                players = Instantiate<GameObject>(sprite3, worldLocation, Quaternion.identity);
+            }
+
+        }
+
+    }
+
+
+    // set min and max for player collision rectangle
+    void SetMinAndMax(Vector3 location)
+    {
+        min.x = location.x-playerColliderHalfWidth;
+        min.y= location.y-playerColliderHalfHeight;
+        max.x = location.x + playerColliderHalfWidth;
+        max.y = location.y-playerColliderHalfHeight;
     }
 }
